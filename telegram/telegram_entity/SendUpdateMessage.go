249ccd3d6c4d09f93831c50ec60887ca816b50fe
TheBestPessimist <cristian@tbp.land>
2018-12-10 15:00:44 +0200

    Disable webpage preview (specifically: disable links) for messages sent to telegram

--- telegram/telegram_entity/SendUpdateMessage.go
+++ telegram/telegram_entity/SendUpdateMessage.go
@@ -1,14 +1,16 @@
 package telegram_entity
 
 type SendUpdateMessage struct {
-	ChatId    int64  `json:"chat_id"`
-	Text      string `json:"text"`
-	ParseMode string `json:"parse_mode"`
+	ChatId                int64  `json:"chat_id"`
+	Text                  string `json:"text"`
+	ParseMode             string `json:"parse_mode"`
+	DisableWebpagePreview bool   `json:"disable_web_page_preview"`
 }
 
 func NewSendUpdateMessage(chatId int64) SendUpdateMessage {
 	return SendUpdateMessage{
-		ChatId:    chatId,
-		ParseMode: "Markdown",
+		ChatId:                chatId,
+		ParseMode:             "Markdown",
+		DisableWebpagePreview: true,
 	}
 }
